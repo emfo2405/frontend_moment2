8eadd662cd2a17c4567062faebc638d77479ff12
emfo2405 <emfo2405@student.miun.se>
2025-02-05 14:29:26 +0100

    data utskriven i tabell

--- src/app.js
+++ src/app.js
@@ -1,23 +1,59 @@
+//Kör funktionen när sidan laddas
+window.onload = fetchCourseData;
+
+//Sparar kursdata i en array
+let courses = [];
+
 // En asynkron funktion som simulerar hämtning av data från en API
-async function fetchData() {
+async function fetchCourseData() {
     try {
+        //Hämtar in data
         const response = await fetch('https://webbutveckling.miun.se/files/ramschema_ht24.json');
-        const data = await response.json();
-        return data;
+//Felmeddelande om datan inte läses in korrekt
+if (!response.ok) {
+    throw new Error("Fel vid anslutning till data...");
+}
+
+//sparar datan till den tomma arrayen
+courses = await response.json();
+
+//Kör funktioner för att skriva ut data
+printCourses(courses);
+
+//Skriver ut datan som en tabell i konsollen
+        console.table(courses);
+
+        //Felmeddelande om något går fel
     } catch (error) {
-        console.error('Fetch error:', error);
-        throw error;
+        console.error(error);
+        document.querySelector("#error").innerHTML = "<p>Något gick fel - prova igen senare</p>";
     }
 }
 
-// Användning av den asynkrona funktionen
-async function processData() {
-    try {
-        const result = await fetchData(); // Väntar på att data ska hämtas
-        console.log('Received data:', result);
-        // ... gör något med den mottagna datan
-    } catch (error) {
-        // Hantera fel om det uppstår vid hämtning eller bearbetning av data
-    }
+function printCourses(data) {
+    const codeEl = document.querySelector("#kurskod");
+    const courseEl = document.querySelector("#kursnamn");
+    const progressionEl = document.querySelector("#progression");
+
+    //Rensa DOM
+    codeEl.innerHTML = "";
+    courseEl.innerHTML = "";
+   progressionEl.innerHTML = "";
+
+    //Filtrera kurskod, kursnamn och progression
+    courses.forEach (course => {
+        codeEl.innerHTML += `<li>${course.code}</li>`;
+    })
+
+    courses.forEach (course => {
+        courseEl.innerHTML += `<li>${course.coursename}</li>`;
+    })
+
+    courses.forEach (course => {
+        progressionEl.innerHTML += `<li>${course.progression}</li>`;
+    })
+
+
+
+
 }
-processData();
