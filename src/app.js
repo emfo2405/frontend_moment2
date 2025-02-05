c4c3066c3372a0828b2ebfed5e846462a016cc37
emfo2405 <emfo2405@student.miun.se>
2025-02-05 17:08:57 +0100

    funktion för att sortera listorna skapade

--- src/app.js
+++ src/app.js
@@ -55,14 +55,17 @@ function printCourses(data) {
 
 }
 
+
+//Tar ut elementet search, som är sökrutan
 const search = document.querySelector("#search");
 
-//Sökfunktion
+//Sökfunktion som aktiveras när besökaren klickar i sökfönstret
 search.onclick = () => {
     document.querySelector("#search"), addEventListener("input", filterCourses);
     printChange(courses);
 }
 
+//Funktion som filtrerar kurser baserat på vad som skrivs in i sökrutan
 function filterCourses() {
     let searchInput = document.querySelector("#search").value;
     let filteredCourses = courses.filter((course) => 
@@ -73,6 +76,7 @@ function filterCourses() {
     printChange(filteredCourses);
 }
 
+//Tar ut kolumnerna, tömmer dem och skriver ut det filtrerade resultatet
 function printChange(courses) {
     const codeEl = document.querySelector("#kurskod");
     const courseEl = document.querySelector("#kursnamn");
@@ -82,19 +86,88 @@ function printChange(courses) {
     courseEl.innerHTML = "";
    progressionEl.innerHTML = "";
 
+   //Skriver ut filtrerade kurskoder
    courses.forEach (course => {
     codeEl.innerHTML += `<li>${course.code}</li>`;
 })
 
+   //Skriver ut filtrerade kursnamn
 courses.forEach (course => {
     courseEl.innerHTML += `<li>${course.coursename}</li>`;
 })
 
+   //Skriver ut filtrerade progressioner
 courses.forEach (course => {
     progressionEl.innerHTML += `<li>${course.progression}</li>`;
 })
 
 }
 
+//Funktion som sorterar listor
+const codeElTitle = document.querySelector("#kurskod_titel");
+const courseElTitle = document.querySelector("#kursnamn_titel");
+const progressionElTitle = document.querySelector("#progression_titel");
 
+//Vid klick på kurskod-titel körs en sortering av kurskoder
+codeElTitle.onclick = () => {
+    document.querySelector("#kurskod_titel"), addEventListener("onclick", sortedCoursesCode(courses));
+    printSorted(courses);
+}
+
+//Vid klick på kursnamn-titel körs en sortering av kursnamn
+courseElTitle.onclick = () => {
+        document.querySelector("#kursnamn_titel"), addEventListener("onclick", sortedCoursesName(courses));
+        printSorted(courses);
+}
+
+//Vid klick på progression-titel körs en sortering av progression
+progressionElTitle.onclick = () => {
+    document.querySelector("#progression_titel"), addEventListener("onclick", sortedProgression(courses));
+    printSorted(courses);
+}
+
+//Sorterar kurskoder
+function sortedCoursesCode(courses) {
+courses.sort((a,b) => a.code > b.code ? 1 : -1);
+printSorted(courses);
+}
+
+//Sorterar kursnamn
+function sortedCoursesName(courses) {
+    courses.sort((a,b) => a.coursename > b.coursename ? 1 : -1);
+    printSorted(courses);
+    }
+
+    //Sorterar progression
+    function sortedProgression(courses) {
+        courses.sort((a,b) => a.progression > b.progression ? 1 : -1);
+        printSorted(courses);
+        }
+
+
+        //Skriver ut de sorterade listorna
+function printSorted(courses) {
+    const codeEl = document.querySelector("#kurskod");
+    const courseEl = document.querySelector("#kursnamn");
+    const progressionEl = document.querySelector("#progression");
+
+    codeEl.innerHTML = "";
+    courseEl.innerHTML = "";
+   progressionEl.innerHTML = "";
+
+   //Skriver ut filtrerade kurskoder
+   courses.forEach (course => {
+    codeEl.innerHTML += `<li>${course.code}</li>`;
+})
+
+   //Skriver ut filtrerade kursnamn
+courses.forEach (course => {
+    courseEl.innerHTML += `<li>${course.coursename}</li>`;
+})
+
+   //Skriver ut filtrerade progressioner
+courses.forEach (course => {
+    progressionEl.innerHTML += `<li>${course.progression}</li>`;
+})
+}
 
